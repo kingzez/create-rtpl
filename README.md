2d7d37d74cd77de7cbe8290a68fc5ee4470a541d
wangzezhi <wangzezhi@dankegongyu.com>
2019-08-16 19:51:07 +0800

    ✨ Feat: init project

---
+++ README.md
@@ -0,0 +1,18 @@
+create-rtpl
+===========
+
+init react template project
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/create-rtpl.svg)](https://npmjs.org/package/create-rtpl)
+[![Downloads/week](https://img.shields.io/npm/dw/create-rtpl.svg)](https://npmjs.org/package/create-rtpl)
+[![License](https://img.shields.io/npm/l/create-rtpl.svg)](https://github.com/kingzez/create-rtpl/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+```shell
+npx create-rtpl
+```
+# Commands
+<!-- commands -->
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import {Command, flags} from '@oclif/command'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as spinner from './spinner'
+import ora from 'ora'
+import chalk from 'chalk'
+
+const welcome = (): void => {
+  const name = chalk.hex('#FEFEFE').bold('react-template')
+  const text = chalk.hex('#F0F0F0')(`  Welcome to ${name} installer`)
+  console.log(text)
+  console.log('')
+}
+
+
+const logColor = (text: string): string => {
+  return chalk.hex('#bdbdbd')(text)
+}
+
+const promptText = (text: string): string => {
+  text = text.replace('project name', chalk.bold('project name'))
+  return logColor(text)
+}
+
+
+class CreateRtpl extends Command {
+  static description = 'init react-template project'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({char: 'n', description: 'name to print'}),
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  async run() {
+    const {args, flags} = this.parse(CreateRtpl)
+    const name = flags.name // ||
+    welcome()
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
+    }
+
+    // input
+    const prompt = require('prompt-sync')()
+    const required = false
+    const str = promptText(`> You need to specify the project name${required ? '(required)' : ''}: `)
+    let project = prompt(str)
+    const projectPath = path.join(process.cwd(), project)
+    if (fs.existsSync(projectPath)) throw new Error(`Abort, "${project}" already exists. Nothing has changed.`)
+    const projectName = project
+
+    // clone
+    spinner.start('template installing...')
+    require('git-clone')(
+      'https://github.com/kingzez/react-template',
+      projectPath,
+      { shallow: true },
+      (err: any) => {
+        if (err) return console.log(new Error(`About. ${err}`))
+        spinner.succeed(true)
+        spinner.start('Installed, enjoy!')
+        spinner.succeed()
+        console.log('')
+      },
+    )
+  }
+}
+
+export = CreateRtpl
